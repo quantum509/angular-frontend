bdebeeb66ca3abfe8d22d33e09f4db2a140d82ab
vmallela0 <princevedu@gmail.com>
2020-05-28 11:16:29 -0400

    Got rid of modeservice
    Deleted mode service but still have it in the login-service.

--- src/login-service/services/login.service.ts
+++ src/login-service/services/login.service.ts
@@ -1,11 +1,141 @@
-import { UserManager, User, UserManagerSettings, UserManagerEvents } from "oidc-client";
-import { Subscription, Subject } from "rxjs";
+import { UserManager, User, UserManagerSettings, UserManagerEvents } from 'oidc-client';
+import { Subscription, Subject } from 'rxjs';
 
-import { Mutex } from "async-mutex";
+import { Mutex } from 'async-mutex';
 
-import { ModeService } from "../services/mode.service";
-import { Injectable } from "@angular/core";
+// import { ModeService } from "../services/mode.service";
+import { Injectable } from '@angular/core';
+import * as querystring from 'querystring'; // see https://nodejs.org/api/querystring.html#querystring_querystring_parse_str_sep_eq_options
 
+
+export class ModeService {
+  private initial_href: string; // original href before angular strips query and rewrites fragment
+  private query_string: string; // query string from original href
+  private _query: any; // query decoded from original href
+  private query_used: any; // query parameter keys used within modeService
+  private query_login: string; // login-related items from query string
+  private fragment: string; // fragment from original href
+  private segment: string; // last segment of fragment from original href
+  private fullUrl: string; // rebuilt canonical URL corresponding to original href
+  private _silent: boolean = false;  // silent mode - callback from silent login
+  private _login: 'NONE' | 'INFO' | 'STATUS' | 'FAILED';   // not actually a mode - indicates login query was given
+  private _iframe: boolean = false;  // not actually a mode - indicates page is loaded in an iframe
+  private _mode: string; // general mode of this invocation
+
+  constructor() {
+      this.initial_href = window.location.href; // TODO: change back to injecting this as soon as that's supported by `ionic build --prod`
+      this._iframe = (parent !== window);
+      this._dissect_initial_href();
+      this._detect_silent();
+      this._detect_login();
+      this._set_mode();
+      this._reload();
+  } 
+  private _dissect_initial_href() {
+      let query_delimiter: number = this.initial_href.indexOf('?');
+      if (query_delimiter <= -1) { query_delimiter = this.initial_href.indexOf('&'); }
+      let fragment_delimiter: number = this.initial_href.indexOf('#');
+      if (query_delimiter > -1) {
+          let query_start = query_delimiter + 1;
+          let query_end = this.initial_href.length;
+          if (fragment_delimiter >= query_start) { query_end = fragment_delimiter; }
+          this.query_string = this.initial_href.slice(query_start, query_end);
+          this._query = querystring.parse(this.query_string);
+          this.query_used = {};
+      }
+      if (fragment_delimiter > -1) {
+          let fragment_start = fragment_delimiter + 1;
+          let fragment_end = this.initial_href.length;
+          if (query_delimiter >= fragment_start) { fragment_end = query_delimiter; }
+          this.fragment = this.initial_href.slice(fragment_start, fragment_end);
+          let segment_delimiter = this.fragment.lastIndexOf('/'); // this should never return -1, but if it does the segment will start at 0
+          let segment_start = segment_delimiter + 1;
+          this.segment = this.fragment.slice(segment_start);
+          // let suffix_delimiter = this.segment.lastIndexOf("-");
+      }
+
+  }
+  private _detect_silent() {
+      if (this._query && this._query.mode === 'silent') {
+          this._silent = true;
+          this.query_used.mode = true;
+      }
+  }
+
+  private _detect_login() {
+      if (this._query) {
+          const login_keys = ['session_state', 'scope', 'state', 'expires_in', 'token_type', 'id_token', 'access_token'];
+          let login: any = {};
+          let missing: boolean = false;
+          for (let key of login_keys) {
+              let value: string = this._query[key];
+              if (!value) {
+                  missing = true;
+                  break;
+              }
+          }
+          if (!missing) {
+              for (let key of login_keys) {
+                  let value: string = this._query[key];
+                  login[key] = value;
+                  this.query_used[key] = true; // second loop required to do this after checking for missing
+              }
+              this._login = 'INFO';
+              if (this._query.mode === 'redirect') { this.query_used.mode = true; }
+              this.query_login = querystring.stringify(login);
+          } else {
+              const status_keys = ['session_state', 'scope', 'state', 'id_token'];
+              login = {};
+              missing = false;
+              for (let key of status_keys) {
+                  let value: string = this._query[key];
+                  if (!value) {
+                      missing = true;
+                      break;
+                  }
+              }
+              if (!missing) {
+                  for (let key of status_keys) {
+                      let value: string = this._query[key];
+                      login[key] = value;
+                      this.query_used[key] = true; // second loop required to do this after checking for missing
+                  }
+                  this._login = 'STATUS';
+                  if (this._query.mode === 'redirect') { this.query_used.mode = true; }
+                  this.query_login = querystring.stringify(login);
+              }
+          }
+      }
+  }
+
+  private _set_mode() {
+      if (this._silent) {
+          this._mode = 'silent';
+      } else if (this._login === 'INFO') {
+          this._mode = 'login';
+      } else {
+          this._mode = 'normal';
+      }
+  }
+
+  private _reload() {
+      let reload: boolean = false;
+      if (reload) {
+          window.location.href = this.fullUrl;
+          window.location.reload();
+      }
+  }
+
+  get silent(): boolean { return this._silent; }
+  get login(): string { return this._login; }
+  get iframe(): boolean { return this._iframe; }
+  get url(): string { return this.fullUrl; }
+  get url_segment(): string { return this.segment; }
+  get login_query(): string { return this.query_login; }
+  get mode(): string { return this._mode; }
+  
+
+}
 @Injectable()
 export class LoginService {
   static get DefaultUserManagerSettings(): UserManagerSettings {
@@ -20,6 +150,7 @@ export class LoginService {
       silentRequestTimeout: 11000
     };
   }
+  
 
   private mgr: UserManager;
   private currentUser: User;
@@ -46,8 +177,8 @@ export class LoginService {
   public get headers(): { [key: string]: string } | undefined {
     if (this.currentUser) {
       return {
-        "Content-Type": "application/json",
-        "Authorization": "Bearer " + this.bearerToken
+        'Content-Type': 'application/json',
+        'Authorization': 'Bearer ' + this.bearerToken
       };
     }
   }
@@ -69,26 +200,25 @@ export class LoginService {
       this.loginConfig = Object.assign({}, LoginService.DefaultUserManagerSettings, loginConfig);
       this.mgr = new UserManager(this.loginConfig);
       this.mgr.events.addUserLoaded((user: User) => this.onUserLoaded(user));
-      this.mgr.events.addAccessTokenExpired(() => this.onUserUnloaded("UserManager emitted AccessTokenExpired"));
-      this.mgr.events.addUserUnloaded(() => this.onUserUnloaded("UserManager emitted UserUnloaded"));
+      this.mgr.events.addAccessTokenExpired(() => this.onUserUnloaded('UserManager emitted AccessTokenExpired'));
+      this.mgr.events.addUserUnloaded(() => this.onUserUnloaded('UserManager emitted UserUnloaded'));
       this.initializeEvent();
       return this.processMode();
     } else if (loginConfig === this.loginConfig) {
       return Promise.resolve();
     } else {
-      return Promise.reject(new Error("Cannot Initialize loginService More Than Once!"));
+      return Promise.reject(new Error('Cannot Initialize loginService More Than Once!'));
     }
   }
 
   private processMode() {
     return this.doSafeUserAction(this.publicLock, () => {
       switch (this.modeService.mode) {
-        case "silent":  // Process silent redirect, no fallback
+        case 'silent':  // Process silent redirect, no fallback
           return this.doSafeUserAction(this.privateLock, () => this.mgr.signinSilentCallback(this.modeService.login_query));
-        case "login":  // Process login redirect, fallback on recovery
+        case 'login':  // Process login redirect, fallback on recovery
           return this.doSafeUserAction(this.privateLock, () => this.mgr.signinRedirectCallback(this.modeService.login_query)).catch(() => this.logInSilent());
-        case "embedded":  // getOrRecover
-        case "normal":
+        case 'normal':
           return this.getOrRecoverUser();
       }
     })
@@ -132,10 +262,10 @@ export class LoginService {
 
   private recoverUserOrLogin(stateinfo?: string | undefined): Promise<User> {
     return this.recoverUser()
-      .then(() => this.getUserOrFail(("User recovered to " + this.currentUser)))
+      .then(() => this.getUserOrFail(('User recovered to ' + this.currentUser)))
       .catch((err: any) => {
         return this.logInInteractive(stateinfo)
-          .then(() => this.getUserOrFail(("Interactive login resolved to " + this.currentUser)));
+          .then(() => this.getUserOrFail(('Interactive login resolved to ' + this.currentUser)));
       });
   }
 
@@ -145,53 +275,44 @@ export class LoginService {
 
   private recoverUser(): Promise<User> {
     return this.logInSilent()
-      .then(() => this.getUserOrFail(("Silent login resolved to " + this.currentUser)));
+      .then(() => this.getUserOrFail(('Silent login resolved to ' + this.currentUser)));
   }
 
   private logInInteractive(stateinfo?: string | undefined): Promise<User> {
-    if (this.modeService.embedded)
-      return Promise.reject("This frame was unable to find your login session.");
-
     return this.logInRedirect(stateinfo)
-      .then(() => this.getUserOrFail(("Redirect login resolved to " + this.currentUser)));
+      .then(() => this.getUserOrFail(('Redirect login resolved to ' + this.currentUser)));
   }
 
   private logInRedirect(stateinfo?: string | undefined): Promise<User | void> {
-    let stateobj: any = stateinfo ? { state: stateinfo } : undefined; // TODO: don't use type any
-    if (this.modeService.embedded)
-      return Promise.reject("This frame was unable to find your login session.");
-
+    let stateobj: any = stateinfo ? { state: stateinfo } : undefined; // TODO: don't use type anyInjectable
+    
     return this.doSafeUserAction(this.privateLock, () => this.mgr.signinRedirect(stateobj))
-      .catch(() => this.getUserOrFail("Redirect login resolved to " + this.currentUser));
+      .catch(() => this.getUserOrFail('Redirect login resolved to ' + this.currentUser));
   }
 
   private logInSilent(): Promise<User> {
     if (this.modeService.silent)
-      return Promise.reject("recursive silent login requested in silent mode.");
+      return Promise.reject('recursive silent login requested in silent mode.');
 
     return this.doSafeUserAction(this.privateLock, () => this.mgr.signinSilent())
-      .catch(() => this.getUserOrFail("Silent login resolved to " + this.currentUser));
+      .catch(() => this.getUserOrFail('Silent login resolved to ' + this.currentUser));
   }
 
   public logOut(): Promise<void> {
-    if (this.modeService.embedded) {
-      return Promise.reject("You must log out of the whole page, not just this frame.");
-    } else {
       return this.doSafeUserAction(this.publicLock, () => this.mgr.signoutRedirect().then(() => this.clearAuth()));
-    }
   }
 
   private onUserLoaded(user: User): void {
     if (!user) {
-      this.userEvents.next(new Error("Invalid user (false) = " + JSON.stringify(user)));
+      this.userEvents.next(new Error('Invalid user (false) = ' + JSON.stringify(user)));
     } else if (user.expired) {
-      this.userEvents.next(new Error("Invalid user (expired," + user.expires_in + ")"));
+      this.userEvents.next(new Error('Invalid user (expired,' + user.expires_in + ')'));
     } else {
       this.doSafeUserAction(this.privateLock, () => {
         this.currentUser = user;
         this.userEvents.next(user);
       }).catch((err: Error) => {
-        err.message = "Failed while or after acquiring userLock -- " + err.message;
+        err.message = 'Failed while or after acquiring userLock -- ' + err.message;
         this.userEvents.next(err);
       });
     }
@@ -205,7 +326,7 @@ export class LoginService {
       }
 
     }).catch((err: Error) => {
-      err.message = "Failed while or after acquiring userLock -- " + err.message;
+      err.message = 'Failed while or after acquiring userLock -- ' + err.message;
       this.userEvents.next(err);
     });
   }
